3646ac41d02eb8d98b80888eac947458b2d8c11a
maryu <huhu_hai_li98@163.com>
2020-08-14 14:25:18 +0800

    ...

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+//sdhafdsafhgsdajhsafshfsajdfhasjfasd
 AppRegistry.registerComponent(appName, () => App);
